bfd9fba4dde9532e5d887857d51aaa81b6b8e830
Daniel <Daniel@DESKTOP-QS97B6P>
2021-10-04 00:26:45 +0100

    Created initial file.

---
+++ Test/src/ie/itcarlow/test/Test.java
@@ -0,0 +1,12 @@
+package ie.itcarlow.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a test");
+
+	}
+
+}
